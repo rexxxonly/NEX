1cb92c12f24ec496b07620baeff1d756538f44bf
rexxxonly <rexxybrexx@gmail.com>
2025-07-07 10:29:05 +0600

    Create particles.js

---
+++ public/particles.js
@@ -0,0 +1,3 @@
+// Get from: https://github.com/VincentGarreau/particles.js
+// Use minified version from CDN:
+// <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
